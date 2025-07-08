0c3c1b11c8254ff1a28498411ca224469df6438f
martinjauma <martin.jauma@gmail.com>
2025-07-08 07:55:21 -0300

    .secret_local

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 import streamlit as st
 import pandas as pd
+import os
 from pymongo import MongoClient
 
 # --- conexión MongoDB ---
 # Accedé al secreto
-mongo_url = st.secrets["MONGO_URL"]
+mongo_url = st.secrets.get("MONGO_URL", os.getenv("MONGO_URL"))
 
 # Conectá a MongoDB
 client = MongoClient(mongo_url)
 db = client["SRA"]
 collection = db["DATA"]
+
+# --- Obtener estadísticas de la colección ---
+stats = db.command("collstats", "DATA")
+
+# Tamaño total en bytes → lo pasamos a MB (dividido 1024²)
+size_mb = stats["size"] / (1024 * 1024)
+
 
 # --- consultamos todos los documentos ---
 # (limitamos por las dudas a 1000 para no reventar la app)
@@ -24,4 +32,6 @@ df = pd.DataFrame(data)
 # --- mostramos con Streamlit ---
 st.title("SRA Data Viewer")
 st.write(f"Documentos en la colección: {collection.count_documents({})}")
+st.write(f"Peso total de los documentos: {size_mb:.2f} MB")
+
 st.dataframe(df)
